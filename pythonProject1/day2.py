ecc103b36ba5638e82fe3fc0481e7c1a534cc1c3
JCking19 <1836627353@qq.com>
2021-06-22 19:28:17 +0800

    day2作业在里面

---
+++ pythonProject1/day2.py
@@ -0,0 +1,229 @@
+id1= "1号"
+name1= "羽绒服"
+price1= 253.6
+num1= 500
+quan1= 10
+
+id2= "2号"
+name2= "牛仔裤"
+price2= 86.3
+num2= 600
+quan2= 60
+
+id3= "3号"
+name3= "风衣"
+price3= 96.8
+num3= 335
+quan3= 43
+
+id4= "4号"
+name4= "皮草"
+price4= 135.9
+num4= 855
+quan4= 63
+
+id5= "5号"
+name5= "T恤"
+price5= 65.8
+num5= 632
+quan5= 63
+
+id6= "6号"
+name6= "衬衫"
+price6=49.3
+num6= 562
+quan6= 120
+
+id7= "7号"
+name7= "牛仔裤"
+price7= 86.3
+num7= 600
+quan7= 72
+
+id8= "8号"
+name8= "羽绒服"
+price8= 253.6
+num8= 500
+quan8= 69
+
+id9= "9号"
+name9= "牛仔裤"
+price9= 86.3
+num9= 600
+quan9= 35
+
+id10= "10号"
+name10= "羽绒服"
+price10= 253.6
+num10= 500
+quan10= 140
+
+id11= "11号"
+name11= "牛仔裤"
+price11= 86.3
+num11= 600
+quan11= 90
+
+id12= "12号"
+name12= "皮草"
+price12= 135.9
+num12= 855
+quan12= 24
+
+id13= "13号"
+name13= "T恤"
+price13= 65.8
+num13= 632
+quan13= 45
+
+id14= "14号"
+name14= "风衣"
+price14= 96.8
+num14= 335
+quan14= 25
+
+id15= "15号"
+name15= "牛仔裤"
+price15= 86.3
+num15= 600
+quan15= 60
+
+id16= "16号"
+name16= "T恤"
+price16= 65.8
+num16= 632
+quan16= 129
+
+id17= "17号"
+name17= "羽绒服"
+price17= 253.6
+num17= 500
+quan17= 10
+
+id18= "18号"
+name18= "风衣"
+price18= 96.8
+num18= 335
+quan18= 43
+
+id19= "19号"
+name19= "T恤"
+price19= 65.8
+num19= 632
+quan19= 63
+
+id20= "20号"
+name20= "牛仔裤"
+price20= 86.3
+num20= 600
+quan20= 60
+
+id21= "21号"
+name21= "皮草"
+price21= 135.9
+num21= 855
+quan21= 63
+
+id22= "22号"
+name22= "风衣"
+price22= 96.8
+num22= 335
+quan22= 60
+
+id23= "23号"
+name23= "T恤"
+price23= 65.8
+num23= 632
+quan23= 58
+
+id24= "24号"
+name24= "牛仔裤"
+price24= 86.3
+num24= 600
+quan24= 140
+
+id25= "25号"
+name25= "T恤"
+price25= 65.8
+num25= 632
+quan25= 48
+
+id26= "26号"
+name26= "风衣"
+price26= 96.8
+num26= 335
+quan26= 43
+
+id27= "27号"
+name27= "皮草"
+price27= 135.9
+num27= 855
+quan27= 57
+
+id28= "28号"
+name28= "羽绒服"
+price28= 253.6
+num28= 500
+quan28= 10
+
+id29= "29号"
+name29= "T恤"
+price29= 65.8
+num29= 632
+quan29= 63
+
+id30= "30号"
+name30= "风衣"
+price30= 96.8
+num30= 335
+quan30= 78
+
+
+
+
+
+print("------------------------Jking服装销售商城----------------------")
+print("-----------------------12月份的衣服销售情况---------------------")
+print("-----日期-----服装名称-----价格-----库存数量-----每日销售量------")
+print(id1,"\t\t",name1,"\t\t",price1,"\t\t",num1,"\t\t",quan1)
+print(id2,"\t\t",name2,"\t\t",price2,"\t\t",num2,"\t\t",quan2)
+print(id3,"\t\t",name3,"\t\t",price3,"\t\t",num3,"\t\t",quan3)
+print(id4,"\t\t",name4,"\t\t",price4,"\t\t",num4,"\t\t",quan4)
+print(id5,"\t\t",name5,"\t\t",price5,"\t\t",num5,"\t\t",quan5)
+print(id6,"\t\t",name6,"\t\t",price6,"\t\t",num6,"\t\t",quan6)
+print(id7,"\t\t",name7,"\t\t",price7,"\t\t",num7,"\t\t",quan7)
+print(id8,"\t\t",name8,"\t\t",price8,"\t\t",num8,"\t\t",quan8)
+print(id9,"\t\t",name9,"\t\t",price9,"\t\t",num9,"\t\t",quan9)
+print(id10,"\t\t",name10,"\t\t",price10,"\t\t",num10,"\t\t",quan10)
+print(id11,"\t\t",name11,"\t\t",price11,"\t\t",num11,"\t\t",quan11)
+print(id12,"\t\t",name12,"\t\t",price12,"\t\t",num12,"\t\t",quan12)
+print(id13,"\t\t",name13,"\t\t",price13,"\t\t",num13,"\t\t",quan13)
+print(id14,"\t\t",name14,"\t\t",price14,"\t\t",num14,"\t\t",quan14)
+print(id15,"\t\t",name15,"\t\t",price15,"\t\t",num15,"\t\t",quan15)
+print(id16,"\t\t",name16,"\t\t",price16,"\t\t",num16,"\t\t",quan16)
+print(id17,"\t\t",name17,"\t\t",price17,"\t\t",num17,"\t\t",quan17)
+print(id18,"\t\t",name18,"\t\t",price18,"\t\t",num18,"\t\t",quan18)
+print(id19,"\t\t",name19,"\t\t",price19,"\t\t",num19,"\t\t",quan19)
+print(id20,"\t\t",name20,"\t\t",price20,"\t\t",num20,"\t\t",quan20)
+print(id21,"\t\t",name21,"\t\t",price21,"\t\t",num21,"\t\t",quan21)
+print(id22,"\t\t",name22,"\t\t",price22,"\t\t",num22,"\t\t",quan22)
+print(id23,"\t\t",name23,"\t\t",price23,"\t\t",num23,"\t\t",quan23)
+print(id24,"\t\t",name24,"\t\t",price24,"\t\t",num24,"\t\t",quan24)
+print(id25,"\t\t",name25,"\t\t",price25,"\t\t",num25,"\t\t",quan25)
+print(id26,"\t\t",name26,"\t\t",price26,"\t\t",num26,"\t\t",quan26)
+print(id27,"\t\t",name27,"\t\t",price27,"\t\t",num27,"\t\t",quan27)
+print(id28,"\t\t",name28,"\t\t",price28,"\t\t",num28,"\t\t",quan28)
+print(id29,"\t\t",name29,"\t\t",price29,"\t\t",num29,"\t\t",quan29)
+print(id30,"\t\t",name30,"\t\t",price30,"\t\t",num30,"\t\t",quan30)
+
+print("12月份销售总金额为:￥",("%.2f" %(price1 * quan1 + price2 * quan2 + price3 * quan3 + price4 * quan4 + price5 * quan5 + price6 * quan6 + price7 * quan7 + price8 * quan8 + price9 * quan9 + price10 * quan10
+                     + price11 * quan11 + price12 * quan12 + price13 * quan13 + price14 * quan14 + price15 * quan15 + price16 * quan16 + price17 * quan17 + price18 * quan18 + price19 * quan19 + price20 * quan20 + price21 * quan21
+                      + price22 * quan22 +  price23 * quan23 + price24 * quan24 + price25 * quan25 + price26 * quan26 + price27 * quan27 + price28 * quan28 + price29 * quan29 + price30 * quan30 )))
+
+print("羽绒服的销售占比为",("%.2f" %(price1*(quan1+quan8+quan10+quan17+quan28)/198400.59*100)))
+print("牛仔裤的销售占比为",("%.2f" %(price2*(quan2+quan7+quan9+quan11+quan15+quan20+quan24)/198400.59*100)))
+print("风衣的销售占比为",("%.2f" %(price3*(quan3+quan14+quan18+quan22+quan26+quan30)/198400.59*100)))
+print("皮草的销售占比为",("%.2f" %(price4*(quan4+quan12+quan21+quan27)/198400.59*100)))
+print("T恤的销售占比为",("%.2f" %(price5*(quan5+quan13+quan16+quan19+quan23+quan25+quan29)/198400.59*100)))
+print("衬衫的销售占比为",("%.2f" %(price6*(quan6)/198400.59*100)))
+
